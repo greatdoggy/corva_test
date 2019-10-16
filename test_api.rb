b1e39c59f49411102d159c7fc82da74045db3aa2
Denys Dymko <denis@rademade.com>
2019-10-16 16:35:34 +0300

    Add errors handling

--- test_api.rb
+++ test_api.rb
@@ -6,6 +6,18 @@ ATTRIBUTES = %w(timestamp data)
 FIRST_PART_TITLE = "Part1"
 SECOND_PART_TITLE = "Part2"
 RESULT_TITLE = "Result"
+INVALID_REQUEST_ERROR  = "Invalid post data"
+INVALID_URL_ERROR      = "Invalid url"
+
+def status_404
+  status 404
+  { "error" => INVALID_URL_ERROR }.to_json
+end
+
+ def status_400
+   status 400
+   { "error" => INVALID_REQUEST_ERROR }.to_json
+ end
 
 def valid_attributes
       # check basic correctnes of input hash
